933c00ff4c6827f73eb30c23281a288824536838
Felipe Ferreira Borges <fferrebo@everis.com>
2020-08-23 15:32:08 -0300

    register

--- src/app/user/register-user/register-user.component.ts
+++ src/app/user/register-user/register-user.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import{ User } from '../model/user.model'
+import { AuthService } from '../../auth/shared/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
@@ -8,7 +13,8 @@ import{ User } from '../model/user.model'
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder, private auhtService:AuthService, private snackBar:MatSnackBar,
+    private router: Router) { }
   formRegister : FormGroup = this.fb.group({
     'firstname':['', [Validators.required]],
     'lastname':['',[Validators.required]],
@@ -22,7 +28,7 @@ export class RegisterUserComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
+
   matchingPasswords(group:FormGroup){
     if(group){
       const password1 = group.controls['password1'].value;
@@ -41,7 +47,20 @@ export class RegisterUserComponent implements OnInit {
         password:this.formRegister.value.password1,
         phone:this.formRegister.value.phone
       }
-      console.log(newUser)
+      this.auhtService.register(newUser).subscribe((u)=>{
+        this.snackBar.open('Criado com sucesso','OK',{
+          duration:2000,
+          verticalPosition:'top'
+        })
+      },
+      (err)=>{
+        console.log(err);
+        this.snackBar.open('Erro ao criar a conta','OK',{
+          duration:2000,
+          verticalPosition:'top'
+        })
+      }
+      )
   }
 
 
