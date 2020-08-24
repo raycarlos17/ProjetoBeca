bba5aaa168c54cf021077c1db37865b7c4ccc4fa
Patrick Almeida Silva <palmeids@everis.com>
2020-08-24 16:30:16 -0300

    setando os valores nos campos

--- src/app/user/perfil/perfil.component.ts
+++ src/app/user/perfil/perfil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../user/model/user.model';
+import { AuthService } from '../../auth/shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -6,8 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  user$: Observable<User>;
+  authenticated$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+  private router:Router) {
+    this.user$ = this.authService.getUser()
+    this.authenticated$ = this.authService.authenticate()
+   }
 
   ngOnInit(): void {
   }
